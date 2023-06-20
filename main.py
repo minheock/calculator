0a450f12ad2f10338e51f98c9aa31dd91e3efaa7
minhyeock <dygks0602@naver.com>
2023-06-20 09:56:32 +0900

    버튼 추가, 클릭시  메시지 박스 출현

--- main.py
+++ main.py
@@ -2,17 +2,31 @@
 
 import sys 
 
-from PyQt5.QtWidgets import QApplication, QWidget
-
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout,QMessageBox
+from PyQt5.QtGui import QIcon
 class Calculator(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
+        self.btn1 = QPushButton('Message', self)
+        self.btn1.clicked.connect(self.activateMessage)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1) #addStretch = 여백 ,, 윗여백
+        vbox.addWidget(self.btn1) #버튼을 가운데로 
+        vbox.addStretch(1) #아랫 여백
+
+        self.setLayout(vbox) # 반영
+
         self.setWindowTitle('계산기')
+        # self.setWindowIcon(QIcon(''))
         self.resize(256,256)
         self.show()
+
+    def activateMessage(self) :
+        QMessageBox.information(self, 'infomation', "Button Clicked!")
 
 # 이 파일을 직접 실행할 시에만 명령을 수행하겠다.
 if __name__=='__main__':
