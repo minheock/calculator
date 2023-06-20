a7581b7fe8aa4d9fa983bdc11f309727a0c70c7d
minhyeock <dygks0602@naver.com>
2023-06-20 11:33:53 +0900

    랜덤번호 입력 기능 추가, 입력된 텍스트 clear 기능 추가

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 
 import sys 
 
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout,QMessageBox, QPlainTextEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout,QMessageBox, QPlainTextEdit, QHBoxLayout
 from PyQt5.QtGui import QIcon
+
+import random # 내 맘대로 로또 추첨 기능 추가할래
+
+
 class Calculator(QWidget):
     def __init__(self):
         super().__init__()
@@ -15,22 +19,32 @@ class Calculator(QWidget):
 
         self.btn1 = QPushButton('Message', self)
         self.btn1.clicked.connect(self.activateMessage)
-
+        self.btn2 = QPushButton('Clear', self)
+        self.btn2.clicked.connect(self.clearMessage)
+        #수평
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+        #수직
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1) 
-        vbox.addWidget(self.btn1) #버튼을 가운데로 
+        vbox.addLayout(hbox) #버튼을 가운데로 
         vbox.addStretch(1) #아랫 여백
 
         self.setLayout(vbox) # 반영
 
-        self.setWindowTitle('계산기')
+        self.setWindowTitle('로또번호 추첨기')
         # self.setWindowIcon(QIcon(''))
         self.resize(256,256)
         self.show()
 
     def activateMessage(self) :
-        # QMessageBox.information(self, 'infomation', "Button Clicked!")
-        self.te1.appendPlainText('button on')
+        lottoNum = random.sample(range(1,46), 6)
+        lottoNum.sort() # 오름차순 정렬
+        self.te1.appendPlainText(str(lottoNum))
+    def clearMessage(self):
+        self.te1.clear()    
 
 # 이 파일을 직접 실행할 시에만 명령을 수행하겠다.
 if __name__=='__main__':
